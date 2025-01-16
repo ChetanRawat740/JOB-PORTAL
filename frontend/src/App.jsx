157fca435d59014f7d0d246fe82c114ad0c8afb7
Chetan Singh <chetanrawat740@gmail.com>
2025-01-16 11:02:15 +0530

    frontend basic project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './components/shared/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-red-500'>lets build Job Portal</h1>
+      <Navbar />
+      
+      
+      
+    </>
+  )
+}
+
+export default App
